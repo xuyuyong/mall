f72ae1a8854f5a94aad2a7705e5431923c65a26d
xuyuyong <3176229153@qq.com>
2018-09-22 16:02:03 +0800

    测试

--- coding/mall_manage_teacher/src/main/java/com/atguigu/controller/IndexController.java
+++ coding/mall_manage_teacher/src/main/java/com/atguigu/controller/IndexController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    @RequestMapping("goto_spu")
+    public String goto_spu() {
+        return "spu";
+    }
 
     /**
      * 跳转首页
